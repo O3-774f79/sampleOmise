21d971687253ed14d859ecd331f55d755b4510bf
ananchai <ananchai.pha@gmail.com>
2018-03-04 19:48:17 +0700

    edit file name

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
 <?php
- require_once dirname(__FILE__).'./omise-php/lib/Omise.php'
+ require_once dirname(__FILE__).'./omise-php/lib/Omise.php';
 
  define('OMISE_API_VERSION', '2015-11-17');
  // define('OMISE_PUBLIC_KEY', 'PUBLIC_KEY');
